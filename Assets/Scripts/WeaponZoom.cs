cbfc9d784f60dc9b7cc2002fd83b67b6a2051dd3
Andrey Kuhorenko <zfesandydaytona@gmail.com>
2021-12-10 17:07:02 +0300

    [Zombie-Runner] weapon zoom sensitivity updated

--- Assets/Scripts/WeaponZoom.cs
+++ Assets/Scripts/WeaponZoom.cs
@@ -1,15 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityStandardAssets.Characters.FirstPerson;
 
 public class WeaponZoom : MonoBehaviour
 {
     [SerializeField] Camera fpsCamera;
     [SerializeField] float zoomedOutFOV = 60f;
     [SerializeField] float zoomedInFOV = 20f;
+    [SerializeField] float zoomedOutSensitivity = 2f;
+    [SerializeField] float zoomedInSensitivity = 0.5f;
+
+    RigidbodyFirstPersonController fpsController;
 
     bool isZoomed = false;
 
+
+    private void Start()
+    {
+        fpsController = GetComponent<RigidbodyFirstPersonController>();
+    }
     private void Update()
     {
         if (Input.GetMouseButtonDown(1))
@@ -25,11 +35,15 @@ public class WeaponZoom : MonoBehaviour
         {
             isZoomed = true;
             fpsCamera.fieldOfView = zoomedInFOV;
+            fpsController.mouseLook.XSensitivity = zoomedInSensitivity;
+            fpsController.mouseLook.YSensitivity = zoomedInSensitivity;
         }
         else
         {
             isZoomed = false;
             fpsCamera.fieldOfView = zoomedOutFOV;
+            fpsController.mouseLook.XSensitivity = zoomedOutSensitivity;
+            fpsController.mouseLook.YSensitivity = zoomedOutSensitivity;
         }
     }
 }
